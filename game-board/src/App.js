3a86b80d494a3635c895b4b80bafa850a9220128
luztherose <luzdelarosaramirez@gmail.com>
2020-10-21 15:29:12 -0400

    pass dynamic infortion with props

--- game-board/src/App.js
+++ game-board/src/App.js
@@ -6,7 +6,22 @@ import Player from './Player';
 function App() {
   return (
     <div className="scoreboard">
-        <Header />
+        <Header 
+          title="scoreboard" 
+          totalPlayers={5} 
+        />
+        {/* Props 
+          Every React component and element can receive a list of attributes called properties (or props). Props are a core concept in React because it's how you get data into a component.
+          You pass props to a component via the component's JXS tag at the place where is used. You can give a prop any name tha you want.
+          Anytime you pass a value other than string, like a number or variable, you place it btw curly braces so that it gets evaluted as a JXS expression.
+          The javaScript we write btw curly braces needs to be an expession or something that returns a value
+          Prop Tips
+          1. When a component has more than one prop, you'll often see them written on separate lines and indented.
+          2. You can omit the value of a prop when it's explicitly true
+          3. Use double quotes (") when writing props. HTML attributes commonly use double quotes instead of single, so props mirror this convention
+        */}
+        
+        
         {/* player list */}
         <Player />
     </div>
--- game-board/src/Header.js
+++ game-board/src/Header.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 // define the component as arrow function
-const Header = () => { // an arrow func expression
+// When you define a component using a function, the function gets one default argument from react, a props object containing a list of props given to the component.
+// To enable the use of props in our Component, we have to give our function a parameter(props). This parameter can be named anything you want, but props is the name most commonly use. 
+const Header = (props) => { // an arrow func expression
+    //console.log(props)
     return ( 
         // if your are returning just JSX, you can use an implicit return
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1 </span>
+        {/* To place the value of the props into the JXS inside the h1 and span, replacing the static text.To access the title and totaPlayers properties of the props obj use use dot notation just as you do this with any object literal in JS.
+        To display the content we use a JXS expression, a JXS expression is sourranded by curly braces.
+        props are "read only" (or immutable), which means that a component can only read the props given to it, never change them. The (parent) component higher in the tree owns and controls the property values. 
+         */}
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers
+} </span>
         </header>
     );
     // including parentheses in JXS in optional

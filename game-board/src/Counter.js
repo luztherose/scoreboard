d106498f05f9cc98407668597b9e33d9f912fcd7
luztherose <luzdelarosaramirez@gmail.com>
2020-10-21 17:00:06 -0400

    create reusable components

--- game-board/src/Counter.js
+++ game-board/src/Counter.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement">-</button>
-            <span className="counter-score">10</span>
+            <span className="counter-score">{props.score}</span>
             <button className="counter-action increment">+</button>
         </div>
     );
--- game-board/src/Player.js
+++ game-board/src/Player.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Counter from './Counter.js';
 
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
             <span className="player-name">
-                Luz
+                {props.playerName}
             </span>
 
-            <Counter /> 
-            {/* Composition is when a component contains others components */}
+            <Counter score={props.score}/> 
+            {/* Composition is when a component contains others components.*/}
+            {/* I'll give the child counter component a prop (of score), the prop can be named anything you want but to be consistent the with prop pass to the player component I named it score.
+            I'll pass the score prompt down to the counter with props.score.
+            Now, we can use the score prop by passing the counter function the parameter props and replacing the static score with curly braces and props.score.
+            Each component in your UI should be responsible for one thing only, and shouldn't contain extra code to handles other things.
+            In other words, each component addresses a specific concern.
+            */}
         </div>
     );
 }

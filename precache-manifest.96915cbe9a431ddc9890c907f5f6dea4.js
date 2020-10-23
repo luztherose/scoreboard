9d6f97e19ac3ba4be528ecc4a8c7a1895379de56
luztherose <luzdelarosaramirez@gmail.com>
2020-10-23 14:10:28 -0400

    Updates

---
+++ precache-manifest.96915cbe9a431ddc9890c907f5f6dea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "043b6c62894c0d717a31594cef13d5e3",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "0967703d81d77ed49f56",
+    "url": "/scoreboard/static/css/main.d3aac498.chunk.css"
+  },
+  {
+    "revision": "7a6cd52450c72bf04560",
+    "url": "/scoreboard/static/js/2.9b552454.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/scoreboard/static/js/2.9b552454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0967703d81d77ed49f56",
+    "url": "/scoreboard/static/js/main.32ec04e1.chunk.js"
+  },
+  {
+    "revision": "07cbc6f8f2c75bdcc95b",
+    "url": "/scoreboard/static/js/runtime-main.a1cbc026.js"
+  }
+]);

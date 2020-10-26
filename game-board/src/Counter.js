d36d67a2cbfd85e1f9decc4178bb873f34ae848c
luztherose <luzdelarosaramirez@gmail.com>
2020-10-26 15:32:03 -0400

    counter feature completed

--- game-board/src/Counter.js
+++ game-board/src/Counter.js
@@ -56,14 +56,51 @@ class Counter extends React.Component {
         state = {
             score:0
         }
-        This class properties syntax is a feature of JavaScript that's currently not support by all browsers
+        This class properties syntax is a feature of JavaScript that's currently not support by all browsers.
+
+        State is local to a component, meaning a component can maintain is own state, unlike props which are read-only.
         */
         super() 
         this.state = {
             /*Since state is data that changes overtime we first name to set an initial state or the state of the component whe it fist mount*/
-            // This state in our counter is going to be the score we want to display for each player
+            // This state in our counter is going to be the score we want to display for each player.
+
+            // To make our counter interactive we need to be able to triger changes to the data in state. The score changes when the user clicks the plus or minus button. First, let's create the function or event handler that update our state using React's built-in set state method. 
+            // Whenever the score gets updated React will re-render our component and the change will be visible in our UI. 
+            // In class components, a common pattern is to create event handlers as a method on the class. 
             score: 0
         };
+    }
+    increamentScore = () =>  { // the name has not special meaning in React
+        //console.log("Hi, from inside increament score!");
+        /*
+        In React, state is never modified directly. The only way React allows you to update a component's state is by using its built-in setState() method.
+        this.setState() let's React knows that state has changed, and that it should re-render and make changes to the component, base on the change in state.
+        You pass setState an object that contains the part of the state you wanna update, and the value you want to update it to. 
+
+                Bind Event Handlers to Components
+        When you create a class component that extends from React.Component, any custom methods you create are not bound to the component by default. You need to bind your custom methods, so that this refers to the component instance. 
+        They are several way to bind thisContext in React. 
+        1. A common way is to call bind in the render method 
+        Each counter component that gets mounted into the dom is an instance of the counter class.
+        In the button's onClick event, I'll call the JavaScript bind method on this.increamentScore and pass it the desired context via this. 
+        this.increamentScore.bind(this)
+
+        2. The other common way to bind event handlers is with an arrow function.
+        So in my button, I'll pass an arrow function to the onClick event that calls this.increamentScore(onClick={() => this.increamentScore()) We don't need to bind this, that's because arrow functions use what's called a lexical this binding which means that it automatically bind them to the scope in which the are defined. 
+        Inside the render method this refers to the counter component instance. The arrow function is enclosed inside the render method, so it takes on the same context. And the this value inside it will properly point to the counter component instance.
+
+        3. The most common way to define a event handler in React is with an arrow function. Arrow functions are automatically bond to the scope in which they are defined. So, if we rewrite the incrementScore method as an arrow function, the function gets bound to the component instance. The arrow function is enclosed inside the counter class, so the context is the component instance. Now we don't need to worry about binding it in the onClick event or in the constructor. We can simply reference and call the function in the onClick event with this.increamentScore make sure there is not parenthenses at the end.
+         */
+        this.setState({
+            score: this.state.score + 1,
+        }); 
+    }
+    
+    decrementScore = () => {
+        this.setState({
+            score: this.state.score -1,
+        });
     }
     render() {
         /*
@@ -71,9 +108,13 @@ class Counter extends React.Component {
          */
         return (
             <div className="counter">
-                <button className="counter-action decrement">-</button>
+            {/* React events are similar to JavaScript events except that they are written inline and named using camelCase.
+            In the increament button, we'll specify the event we are listening for, onClick.(This event is specific to React, so you must name it onClick)
+            You pass React events JXS expressions, using curly braces and the event handler that will get called when the specified event happens. We don't use parentheses to all increamentScore like we usually do to call functions or methods in JavaScript. We are only passing a reference to the method. Adding parentheses will call increamentScore and make it run right when the component mounts, or gets displayed on the page, which we don't want. We want React to call increamentScore only when the onClick event is fired.
+             */}
+                <button className="counter-action decrement" onClick={ this.decrementScore }>-</button>
                 <span className="counter-score">{ this.state.score }</span>
-                <button className="counter-action increment">+</button>
+                <button className="counter-action increment" onClick={this.increamentScore }>+</button>
             </div> 
         );
     }
